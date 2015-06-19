e6a74f4720c445c53ced7a97526ea0d9af7d8ec1
Nicholas Toulouse & Tanay Arora <pair+nicoto.tarora2014@devbootcamp.com>
2015-06-18 17:56:06 -0700

    css for signin page

---
+++ app/controllers/signin.rb
@@ -0,0 +1,22 @@
+# HTML form to sign up
+get '/signin' do
+  erb :signin
+end
+
+post '/signin' do
+  @user = User.where(username: params[:username]).first if params[:username]
+  if @user && @user.password_hash == params[:password_hash]
+    session[:user_id] = @user.id
+    redirect to '/'
+  end
+  @errors = "Your login info was incorrect! Please try again!"
+  erb :signin
+end
+
+post '/signout' do
+  if params[:signout]
+    session[:user_id] = nil
+  end
+  redirect to '/'
+end
+
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,8 @@
 class Answer < ActiveRecord::Base
   # Remember to create a migration!
+
+  validates :content, presence: true
+
   belongs_to :question
   has_many :select_answers
 
